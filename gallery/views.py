c4c6716e79accee46baff1aff9f830dd4abceb01
feliciathebeen <feliciathebeen@gmail.com>
2024-10-02 18:16:07 +0900

    delete

--- gallery/views.py
+++ gallery/views.py
@@ -20,17 +20,4 @@ def index(request):
     return render(request, 'gallery/index.html')
 
 
-def get_post(request):
-    databases = ['default', 'postgres'] ##, 'mysql'
-    for db in databases:
-        try:
-            # 기본적으로 'default'에서 먼저 시도
-            posts = Post.objects.using(db).all()
-            if posts.exists():
-                return JsonResponse({'posts': list(posts.values())})
-        except Exception as e:
-            # 읽기에 실패하면 다음 데이터베이스로 넘어감
-            print(f"Failed to read from database {db}: {e}")
-            continue
-    return JsonResponse({'error': 'No data found in any database'}, status=404)
 
